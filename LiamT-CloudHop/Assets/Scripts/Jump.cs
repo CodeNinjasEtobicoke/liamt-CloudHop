54447b66650dc43f2b2876d4ac1b846f1bcd1d41
Geo_Pro_09 <astrobrandon2013@gmail.com>
2025-04-01 17:48:16 -0400

    prove your self
    xexerer

---
+++ LiamT-CloudHop/Assets/Scripts/Jump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private float jumpForce = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButtonDown("Jump") && (rb.velocity.y < 0.1f && rb.velocity.y > -0.1f))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        }
+    }
+}
